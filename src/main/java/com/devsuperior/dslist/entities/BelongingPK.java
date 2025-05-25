0f7fa86a40b4f8be0c77e0f011544d352876ef90
Gustavo Moura <gustavomourac@outlook.com>
2025-05-25 16:13:41 -0300

    seed update

--- src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -8,10 +8,12 @@ import jakarta.persistence.ManyToOne;
 
 @Embeddable
 public class BelongingPK {
-	@ManyToOne @JoinColumn(name ="game_id")
-	private Game game;
+	
 	@ManyToOne @JoinColumn(name ="list_id")
 	private GameList list;
+	@ManyToOne @JoinColumn(name ="game_id")
+	private Game game;
+
 	
 	public BelongingPK() {
 	}
